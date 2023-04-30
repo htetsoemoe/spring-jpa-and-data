302491e4e07fcdd0cca75bcfa793deb5c3b67f6a
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-04-30 17:16:49 +0630

    created entity relationships

---
+++ getting-start-jpql/src/main/java/com/jdc/jpql/entity/Township.java
@@ -0,0 +1,51 @@
+package com.jdc.jpql.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "township")
+public class Township implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(nullable = false)
+	private String name;
+	@Column(nullable = false)
+	private String division;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDivision() {
+		return division;
+	}
+
+	public void setDivision(String division) {
+		this.division = division;
+	}
+
+}
