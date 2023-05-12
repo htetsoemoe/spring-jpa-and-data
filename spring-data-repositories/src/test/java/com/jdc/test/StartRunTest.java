1abe5961ee3afc9ddaf7a60b2f09b7000fe7195b
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-05-12 22:14:58 +0630

    project setup and run

---
+++ spring-data-repositories/src/test/java/com/jdc/test/StartRunTest.java
@@ -0,0 +1,16 @@
+package com.jdc.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import com.jdc.location.config.JpaConfiguration;
+
+@SpringJUnitConfig(classes = JpaConfiguration.class)
+public class StartRunTest {
+	
+	@Test
+	void test() {
+		
+	}
+
+}
