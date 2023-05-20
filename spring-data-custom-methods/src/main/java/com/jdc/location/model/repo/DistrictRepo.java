b6daa0025738d8aa89a50eb29c9b2779d3b0be60
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-05-20 12:03:03 +0630

    predicate keywords and namedQuery

--- spring-data-custom-methods/src/main/java/com/jdc/location/model/repo/DistrictRepo.java
+++ spring-data-custom-methods/src/main/java/com/jdc/location/model/repo/DistrictRepo.java
@@ -1,9 +1,22 @@
 package com.jdc.location.model.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.jdc.location.model.entity.District;
 
 public interface DistrictRepo extends JpaRepository<District, Integer>{
+	
+	List<District> findByStateRegion(String region);
+	
+	List<District> findByNameStartingWithIgnoringCaseOrderByName(String name);
+	
+	List<District> findByStateIdAndNameStartingWithIgnoringCaseOrderByName(int stateId, String name);
+	
+	@Query(name = "District.findForState")
+	List<District> findWithIdAndNameQuery(@Param("stateId") int id, @Param("name") String name);
 
 }
---
+++ spring-data-custom-methods/src/test/java/com/jdc/test/DistrictRepoTest.java
@@ -0,0 +1,60 @@
+package com.jdc.test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import com.jdc.location.config.JpaConfiguration;
+import com.jdc.location.model.repo.DistrictRepo;
+
+@SpringJUnitConfig(classes = JpaConfiguration.class)
+public class DistrictRepoTest {
+	
+	@Autowired
+	private DistrictRepo repo;
+	
+	@Disabled
+	@ParameterizedTest
+	@CsvSource({
+		"West,9"
+	})
+	void test_find_by_state_region(String region, int size) {
+		var districts = repo.findByStateRegion(region);
+		assertThat(districts, hasSize(size));
+	}
+	
+	@Disabled
+	@ParameterizedTest
+	@CsvSource({
+		"Hpa,2"
+	})
+	void test_find_by_name_with(String name, int size) {
+		var lists = repo.findByNameStartingWithIgnoringCaseOrderByName(name);
+		assertThat(lists, hasSize(size));
+	}
+	
+	@Disabled
+	@ParameterizedTest
+	@CsvSource({
+		"13,mo,3"
+	})
+	void test_find_by_state_and_name(int stateId, String name, int size) {
+		var lists = repo.findByStateIdAndNameStartingWithIgnoringCaseOrderByName(stateId, name);
+		assertThat(lists, hasSize(size));
+	}
+	
+	@ParameterizedTest
+	@CsvSource({
+		"13,mo,3"
+	})
+	void test_find_by_named_query(int stateId, String name, int size) {
+		var lists = repo.findWithIdAndNameQuery(stateId, name.concat("%"));
+		assertThat(lists, hasSize(size));
+	}
+
+}
